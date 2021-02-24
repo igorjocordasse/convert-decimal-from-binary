81d3be0c7d1722b21aa608ce9b2b50ff51afbbab
igor jocordasse <jocordasseigor@gmail.com>
2021-02-24 14:58:57 +0200

    init

---
+++ app.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	var number int
+	fmt.Println("Introdution namber you what to be modifiled ")
+	fmt.Scan(&number)
+
+
+	fmt.Printf("Decimal %d - Binary %b \n", number, number)
+}
